e102deb632601f745e17ebd7b2196ada8c91122f
Lobymaru <Lobymaru@gmail.com>
2023-04-15 18:16:23 -0300

    agregada la libreria jsPDF. Implementada la version preliminar para generar el pdf con el formulario wizard en el modal

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ var btn_cancel = document.getElementById("cancelar");
 var pages = document.getElementsByClassName("modal-precompra");
 
 // Cuando se haga click en el boton, abrir el modal
-btn.onclick = function() {
+function openModal() {
     for(let i = 0; i < pages.length; i++){
         pages[i].style.left = String(100 * i) +"%";
     }
@@ -40,4 +40,29 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}	
+}
+
+
+// Creacion y manipulacion del PDF
+function createRecipe(){
+
+  let nombre = document.getElementById("campoNombre").value;
+  let apellido = document.getElementById("campoApellido").value;
+  let mail = document.getElementById("campoMail").value;
+  let nacimiento = document.getElementById("campoNacimiento").value;
+  let pais = document.getElementById("campoPais").value;
+  let ciudad = document.getElementById("campoCiudad").value;
+  let calle = document.getElementById("campoCalle").value;
+  let telefono = document.getElementById("campoTelefono").value;
+
+  const doc = new jsPDF({
+    orientation:"portrait",
+    unit:"mm",
+    format:"a5",
+    setFontSize: 9
+  });
+
+  doc.text("Gracias por reservar el Metal Gear: Peace Walker! \nguarde este recivo para reclamar su reserva el día\ndel lanzamiento", 10, 10);
+  doc.text("Nombre: " + apellido + " " + nombre + "\nFecha de Nacimiento: " + nacimiento + "\nE-mail: " + mail, 10, 35);
+  doc.save("comprobante.pdf");
+}
