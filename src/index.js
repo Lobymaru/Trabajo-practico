4631da136ffb62d06554c2a76712cd597a1f2baf
Lobymaru <Lobymaru@gmail.com>
2023-04-16 17:26:55 -0300

    Agregada las validaciones basicas para el formulario del modal. Separado el html,css y js del modal en archivos apartes

--- src/index.js
+++ src/index.js
@@ -23,19 +23,88 @@ function closeModal() {
   modal.style.display = "none";
 }
 
+// Validar campos
+
+$("#formularioWizard01").validate({
+  rules:{
+    nombre:{
+      required:true,
+      minlength:3,
+      maxlength:30
+    },
+    apellido:{
+      required:true,
+      minlength:3,
+      maxlength:30
+    },
+    mail:{
+      required:true,
+      email:true
+    },
+    nacimiento:{
+      required:true,
+      dateISO:true
+    }
+  }
+})
+
+$("#formularioWizard02").validate({
+  rules:{
+    ciudad:{
+      required:true,
+      minlength:3,
+      maxlength:30
+    },
+    calle:{
+      required:true,
+      minlength:3,
+      maxlength:30
+    },
+    telefono:{
+      required:true,
+      minlength:3,
+      maxlength:30,
+      digits: true
+    }
+  }
+})
+
+$("#siguienteUno").click(function() {
+  
+  if($("#formularioWizard01").valid() == false){
+    return
+  }
+
+  nextPage();
+
+})
+
+$("#siguienteDos").click(function(){
+  
+  if($("#formularioWizard02").valid() == false){
+    return
+  }
+
+  nextPage();
+
+
+})
+
+// Movimiento de las paginas del modal
+
 function nextPage() {
 
-  let nombre = document.getElementById("campoNombre").value;
-  let apellido = document.getElementById("campoApellido").value;
-  let mail = document.getElementById("campoMail").value;
-  let nacimiento = document.getElementById("campoNacimiento").value;
-  let pais = document.getElementById("campoPais").value;
-  let ciudad = document.getElementById("campoCiudad").value;
-  let calle = document.getElementById("campoCalle").value;
-  let telefono = document.getElementById("campoTelefono").value;
+  let nombre = $("#campoNombre").val();
+  let apellido = $("#campoApellido").val();
+  let mail = $("#campoMail").val();
+  let nacimiento = $("#campoNacimiento").val();
+  let pais = $("#campoPais").val();
+  let ciudad = $("#campoCiudad").val();
+  let calle = $("#campoCalle").val();
+  let telefono = $("#campoTelefono").val();
 
   for (let i = 0; i < pages.length; i++) {
-    pages[i].style.left = String(parseInt(pages[i].style.left) - 100) + "%";
+    pages[i].style.left = String(parseInt(pages[i].style.left) - 100) + "%"; // Desplaza a la izquierda las paginas del modal 
   }
 
   document.getElementById("recivoNombre").innerHTML = "Nombre: " + apellido + " " + nombre;
@@ -65,20 +134,13 @@ window.onclick = function (event) {
 function createRecipe() {
 
   html2canvas(document.getElementById("recivo")).then(function (canvas) {  //Funcion que toma una captura del elemento html indicado y la convierte en una imagen
-      var img = canvas.toDataURL("image/png");  //Indicamos el tipo de imagen de la captura
-      var doc = new jsPDF({
-        orientation: "l",
-        unit: "cm",
-        format:"a6"
-      });  //Creamos el documento PDF
-      doc.addImage(img, "JPEG", 0, 0);  //Agregamos la imagen al documento PDF indicando el tipo y los margenes X e Y
-      doc.save("comprobante.pdf")  //Guardamos el PDF generado
-    });
-
-  
-  /*doc.text("Gracias por reservar el Metal Gear: Peace Walker! \nguarde este recivo para reclamar su reserva el día\ndel lanzamiento", 10, 10);
-  doc.text("Nombre: " + apellido + " " + nombre + "\nFecha de Nacimiento: " + nacimiento + "\nE-mail: " + mail, 10, 35);*/
-
-
-
+    var img = canvas.toDataURL("image/png");  //Indicamos el tipo de imagen de la captura
+    var doc = new jsPDF({
+      orientation: "l",
+      unit: "cm",
+      format: "a6"
+    });  //Creamos el documento PDF
+    doc.addImage(img, "JPEG", 0, 0);  //Agregamos la imagen al documento PDF indicando el tipo y los margenes X e Y
+    doc.save("comprobante.pdf")  //Guardamos el PDF generado
+  });
 }
